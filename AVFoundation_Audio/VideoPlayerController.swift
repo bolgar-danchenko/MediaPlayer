216c78bae45e56af86d6ec5bff01c4a381bb4169
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2022-12-12 18:01:30 +0400

    Names for videos in tableView. README.md added

--- AVFoundation_Audio/VideoPlayerController.swift
+++ AVFoundation_Audio/VideoPlayerController.swift
@@ -5,49 +5,44 @@
 //  Created by Konstantin Bolgar-Danchenko on 14.10.2022.
 //
 
+import Foundation
 import UIKit
 import YouTubePlayerKit
 
 class VideoPlayerController: UITableViewController {
 
-    var videos: [String] = ["psL_5RIBqnY", "-rAeqN-Q7x4", "w87fOAG8fjk"]
+    var videos: [String: String] = ["WWDC 2019" : "psL_5RIBqnY", "September Event 2019" : "-rAeqN-Q7x4", "WWDC 2014" : "w87fOAG8fjk"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    
     }
 
     // MARK: - Table view data source
 
-    override func tableView(
-        _ tableView: UITableView,
-        numberOfRowsInSection section: Int
-    ) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
         return videos.count
     }
 
-    override func tableView(
-        _ tableView: UITableView,
-        cellForRowAt indexPath: IndexPath
-    ) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: "Cell",
-            for: indexPath
-        )
-
-        cell.textLabel?.text = videos[indexPath.row]
-
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath)
+        
+        cell.textLabel?.text = Array(videos)[indexPath.row].key
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
         tableView.deselectRow(at: indexPath, animated: true)
-
-        let player = YouTubePlayer(source: .video(id: self.videos[indexPath.row]), configuration: .init(autoPlay: true))
-        let youTubePlayerController = YouTubePlayerViewController(player: player)
-        self.present(youTubePlayerController, animated: true)
+        
+        let videoID = Array(videos)[indexPath.row].value
+        
+        DispatchQueue.main.async {
+            let player = YouTubePlayer(source: .video(id: videoID), configuration: .init(autoPlay: true))
+            let youTubePlayerController = YouTubePlayerViewController(player: player)
+            self.present(youTubePlayerController, animated: true)
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,19 @@
+# MediaPlayer
+
+Homework for Netology AVFoundation lesson
+
+## Features:
+* Music player
+* YouTube video player
+* Voice recorder
+
+Music trachs are stored in the project.
+Videos from YouTube are playing with YouTubePlayerKit framework installed via Swift Package Manager.
+Voice recordings are stored in FileManager.
+
+## Skills
+
+* AVFoundation
+* AVAudioRecorder
+* Frameworks
+* Storyboard
